d9b126ab457ef879912c58ec2f977f7db432aab9
Justin Chadwell <jedevc@gmail.com>
2018-11-10 19:03:16 +0000

    Separate handlers into other file

---
+++ src/handlers.js
@@ -0,0 +1,31 @@
+function attachHandlers(instrument, element) {
+  let pauseTimeout = null;
+
+  let mouseDown = false;
+  element.addEventListener('mousedown', (event) => {
+    const rect = element.getBoundingClientRect();
+    const y = 1.0 - (event.y - rect.top) / element.clientHeight;
+    const x = (event.x - rect.left) / element.clientWidth;
+    instrument.play(y, x);
+
+    mouseDown = true;
+    clearTimeout(pauseTimeout);
+  })
+  document.addEventListener('mouseup', (event) => {
+    mouseDown = false;
+    pauseTimeout = setTimeout(() => instrument.pause(), 100)
+  })
+
+  element.addEventListener('mousemove', (event) => {
+    if (mouseDown) {
+      const rect = element.getBoundingClientRect();
+      const y = 1.0 - (event.y - rect.top) / element.clientHeight;
+      const x = (event.x - rect.left) / element.clientWidth;
+      instrument.play(y, x);
+    }
+  })
+}
+
+module.exports = {
+  attachHandlers: attachHandlers
+}
--- src/main.js
+++ src/main.js
@@ -1,34 +1,7 @@
 const {Instrument} = require('./instrument');
+const {attachHandlers} = require('./handlers');
 
 window.onload = function() {
   var instrument = new Instrument();
   attachHandlers(instrument, document.getElementById('slider'));
-}
-
-function attachHandlers(instrument, element) {
-  let pauseTimeout = null;
-
-  let mouseDown = false;
-  element.addEventListener('mousedown', (event) => {
-    const rect = element.getBoundingClientRect();
-    const y = 1.0 - (event.y - rect.top) / element.clientHeight;
-    const x = (event.x - rect.left) / element.clientWidth;
-    instrument.play(y, x);
-
-    mouseDown = true;
-    clearTimeout(pauseTimeout);
-  })
-  document.addEventListener('mouseup', (event) => {
-    mouseDown = false;
-    pauseTimeout = setTimeout(() => instrument.pause(), 100)
-  })
-
-  element.addEventListener('mousemove', (event) => {
-    if (mouseDown) {
-      const rect = element.getBoundingClientRect();
-      const y = 1.0 - (event.y - rect.top) / element.clientHeight;
-      const x = (event.x - rect.left) / element.clientWidth;
-      instrument.play(y, x);
-    }
-  })
 }
