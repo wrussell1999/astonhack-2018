6f15e78345e93da195fcdcdb4e34e1b9bf69ed9c
wrussell1999 <jetpowerss@gmail.com>
2018-11-10 22:09:13 +0000

    Lets merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: ['./src/main.js', './css/main.css'],
@@ -16,6 +17,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new FaviconsWebpackPlugin('./my-logo.png'),
     new HtmlWebpackPlugin({
       template: 'index.html'
     })
