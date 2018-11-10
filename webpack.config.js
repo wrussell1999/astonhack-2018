916a217f55978e629a1e8ef712182529fcc8202a
Justin Chadwell <jedevc@gmail.com>
2018-11-10 13:57:57 +0000

    Add css

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ['./src/main.js', './css/main.css'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
