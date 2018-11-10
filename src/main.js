19fff235a9c96c008e315948818a2fc3045bcb30
Justin Chadwell <jedevc@gmail.com>
2018-11-10 14:34:30 +0000

    Timeout after 2secs

--- src/main.js
+++ src/main.js
@@ -17,22 +17,29 @@ class Instrument {
 
     this.oscillator = this.ctx.createOscillator();
     this.oscillator.type = waveType;
+
     this.volume = this.ctx.createGain();
+    this.volume.gain.setValueAtTime(0, this.ctx.currentTime);
 
     this.oscillator.connect(this.volume);
     this.volume.connect(this.ctx.destination);
 
-    // this.volume.gain.setValueAtTime(0.2, this.ctx.currentTime);
+    this.oscillator.start();
 
-    this.playing = false;
+    this.playing = null;
   }
 
   play(freq) {
     this.oscillator.frequency.setValueAtTime(freq, this.ctx.currentTime);
+    this.volume.gain.setValueAtTime(0.2, this.ctx.currentTime);
 
-    if (!this.playing) {
-      this.oscillator.start();
-      this.playing = true;
+    if (this.playing) {
+      clearTimeout(this.playing);
     }
+
+    this.playing = setTimeout(() => {
+      this.volume.gain.setValueAtTime(0, this.ctx.currentTime);
+      this.playing = null;
+    }, 2000);
   }
 }
